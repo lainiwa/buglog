c0451901727ccc0b1429d1225511e88431bba81c
lainiwa <kirrik96@gmail.com>
2020-07-07 16:22:27 +0300

    Resolve conflict with importlib.metadata for different python versions

--- buglog/__init__.py
+++ buglog/__init__.py
@@ -1,3 +1,7 @@
-from importlib.metadata import version
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version
+
 
 __version__ = version(__package__)
